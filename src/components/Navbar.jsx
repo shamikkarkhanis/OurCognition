eb2d85735a73bae0a42f2b3132921384d5469df6
nicod05 <nicolasdiaz.55@icloud.com>
2025-02-21 03:51:48 -0500

    small changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -29,7 +29,7 @@ function Navbar() {
       <nav className='navbar'>
         <div className='navbar-container'>
           <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
-          <img src='/images/ourcognition-logo.png' alt='OurCognition Logo' className="logo" />
+          <img src='/images/logo_form.png' alt='OurCognition Logo' className="logo" />
             <i class='fab fa-typo3' />
           </Link>
           <div className='menu-icon' onClick={handleClick}>
