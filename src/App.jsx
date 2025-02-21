b80b46139f646d6616c5c632d14f4a364847ded9
nicod05 <nicolasdiaz.55@icloud.com>
2025-02-21 02:37:06 -0500

    fixed some things and trying jsx files?

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import Navbar from "./components/Navbar";
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
+import Content from "./components/pages/Content";
+import About from "./components/About";
+import Contact from "./components/Contact";
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/content' element={<Content />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/sign-up' element={<Contact />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
