a75671a17d36bec6ce11ee8d8a35d3ecbeb1fb2b
nicod05 <nicolasdiaz.55@icloud.com>
2025-02-21 02:04:52 -0500

    public set

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import BrainHeatmap from "./components/BrainHeatmap.jsx";
 import Content from "./components/Content.jsx";
