84636848f14f400df9e2b6716b59b5a07c3ca4ce
nicod05 <nicolasdiaz.55@icloud.com>
2025-02-21 12:07:23 -0500

    finished framework

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,26 @@
 import React from 'react';
-import Navbar from "./components/Navbar";
+import Navbar from './components/Navbar';
+import Footer from './components/Footer'; // Assuming you have a Footer component
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/pages/Home';
-import Content from "./components/pages/Content";
-import About from "./components/pages/About";
-import Contact from "./components/pages/Contact";
+import Content from './components/pages/Content';
+import About from './components/pages/About';
+import Contact from './components/pages/Contact';
 
 function App() {
   return (
     <Router>
       <Navbar />
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/content' element={<Content />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/contact' element={<Contact />} />
-      </Routes>
+      <div className="content-wrapper"> {/* Wrapper for content pages */}
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/content' element={<Content />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/contact' element={<Contact />} />
+        </Routes>
+      </div>
+      <Footer /> {/* Footer visible on all pages */}
     </Router>
   );
 }
