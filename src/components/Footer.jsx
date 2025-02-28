cddb1221e7f437a549850ecb4b51bac8b7773d38
nicod05 <nicolasdiaz.55@icloud.com>
2025-02-28 12:46:14 -0500

    updated footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,37 +6,15 @@ import { Link } from 'react-router-dom';
 function Footer() {
   return (
     <div className='footer-container'>
-      <section className='footer-subscription'>
-        <p className='footer-subscription-heading'>
-          Join Our Newsletter to Stay Updated on Alzheimer's Research
-        </p>
-        <p className='footer-subscription-text'>
-          You can unsubscribe at any time.
-        </p>
-        <div className='input-areas'>
-          <form>
-            <input
-              className='footer-input'
-              name='email'
-              type='email'
-              placeholder='Your Email'
-            />
-            <Button buttonStyle='btn--outline'>Subscribe</Button>
-          </form>
-        </div>
-      </section>
       <div className='footer-links'>
         <div className='footer-link-wrapper'>
           <div className='footer-link-items'>
             <h2>About Us</h2>
             <Link to='/about'>Our Mission</Link>
-            <Link to='/research'>Research Areas</Link>
-            <Link to='/team'>Our Team</Link>
           </div>
           <div className='footer-link-items'>
             <h2>Contact Us</h2>
             <Link to='/contact'>Contact</Link>
-            <Link to='/support'>Support</Link>
           </div>
         </div>
         <div className='footer-link-wrapper'>
