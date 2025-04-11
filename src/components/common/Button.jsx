714cd982550f404914b95df56272830a6e81bab7
nicod05 <nicolasdiaz.55@icloud.com>
2025-04-11 11:48:13 -0400

    updated button

--- src/components/common/Button.jsx
+++ src/components/common/Button.jsx
@@ -1,21 +1,8 @@
-// import React from 'react';
-// import './Button.css';
-// import { Link } from 'react-router-dom';
-
-// export function Button() {
-//   return (
-//     <Link to='sign-up'>
-//       <button className='btn'>Sign Up</button>
-//     </Link>
-//   );
-// }
-
 import React from 'react';
 import '../../styles/Button.css';
 import { Link } from 'react-router-dom';
 
 const STYLES = ['btn--primary', 'btn--outline', 'btn--test'];
-
 const SIZES = ['btn--medium', 'btn--large'];
 
 export const Button = ({
@@ -23,23 +10,25 @@ export const Button = ({
   type,
   onClick,
   buttonStyle,
-  buttonSize
+  buttonSize,
+  to 
 }) => {
-  const checkButtonStyle = STYLES.includes(buttonStyle)
-    ? buttonStyle
-    : STYLES[0];
-
+  const checkButtonStyle = STYLES.includes(buttonStyle) ? buttonStyle : STYLES[0];
   const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0];
 
-  return (
-    <Link to='/sign-up' className='btn-mobile'>
-      <button
-        className={`btn ${checkButtonStyle} ${checkButtonSize}`}
-        onClick={onClick}
-        type={type}
-      >
+  return to ? (
+    <Link to={to} className='btn-mobile'>
+      <button className={`btn ${checkButtonStyle} ${checkButtonSize}`} type={type}>
         {children}
       </button>
     </Link>
+  ) : (
+    <button
+      className={`btn ${checkButtonStyle} ${checkButtonSize}`}
+      onClick={onClick}
+      type={type}
+    >
+      {children}
+    </button>
   );
 };
